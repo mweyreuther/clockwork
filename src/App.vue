6fa447c2a985929d2dddeb2aeeb796ae27d995ed
mweyreuther <mweyreuther@me.com>
2022-02-25 20:57:08 +0100

    testing positions

--- src/App.vue
+++ src/App.vue
@@ -26,16 +26,47 @@ export default {
   data() {
     return {
       panel: [],
+      currenTime: null,
     };
   },
   mounted() {
-    const row = Array.from({ length: 4 }, (x, i) => '22:15');
+    const row = Array.from({ length: 4 }, (x, i) => {
+      const now = new Date();
+      return `${now.getHours()}:${now.getMinutes()}`;
+    });
     const panel = Array.from({ length: 6 }, (x, i) => [...row]);
     this.panel = panel;
+    setInterval(() => {
+      const row = Array.from({ length: 4 }, (x, i) => {
+        const now = new Date();
+        return `${now.getHours()}:${now.getMinutes()}`;
+      });
+      const panel = Array.from({ length: 6 }, (x, i) => [...row]);
+      this.panel = panel;
+    }, 60000);
   },
   methods: {
     click([i, ii]) {
-      this.panel[i][ii] = '13:05';
+      // this.panel[i][ii] = '14:34';
+      this.panel[2][1] = '15:30';
+      this.panel[2][2] = '21:30';
+      this.panel[3][1] = '12:15';
+      this.panel[3][2] = '21:00';
+
+      this.panel[1][0] = '15:30';
+      this.panel[1][1] = '21:15';
+      this.panel[1][2] = '21:15';
+      this.panel[1][3] = '21:30';
+
+      this.panel[2][0] = '12:30';
+      this.panel[3][0] = '12:30';
+      this.panel[2][3] = '12:30';
+      this.panel[3][3] = '12:30';
+
+      this.panel[4][0] = '12:15';
+      this.panel[4][1] = '21:15';
+      this.panel[4][2] = '21:15';
+      this.panel[4][3] = '12:45';
     },
   },
 };
